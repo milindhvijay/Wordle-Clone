8aad36730f6c5b65a394986879a4b0e30ed0c4e3
Milindh Vijay Shajiv Nair <milindhvijay@gmail.com>
2022-02-17 18:19:57 +0530

    added animation and color

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let availableSpace = 1;
 
     let word = "dairy";
+    let guessedWordCount = 0;
 
     const keys = document.querySelectorAll('.keyboard-row button')
 
@@ -26,20 +27,52 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
+    function getTileColor(letter, index) {
+        const isCorrectLetter = word.includes(letter);
+
+        if(!isCorrectLetter) {
+            return "rgb(58, 58, 60)";
+        }
+
+        const letterInPosition = word.charAt(index);
+        const isCorrectPosition = letter = letterInPosition;
+
+        if(isCorrectPosition) {
+            return "rgb(83, 141, 78)";
+        }
+
+        return "rgb(181, 159, 59)";
+    }
+
     function handleSubmitWord() {
         const currentWordArray = getCurrentWordArray();
         if (currentWordArray.length !== 5) {
             window.alert("Word must be 5 letters");
         }
 
-        const currentWord = currentWordArray.join(""); 
+        const currentWord = currentWordArray.join("");
+        
+        const firstLetterId = guessedWordCount * 5 + 1;
+        const interval = 200;
+        currentWordArray.forEach((letter, index) => {
+            setTimeout(() => {
+                const tileColor = getTileColor(letter, index);
+
+                const letterId = firstLetterId + index;
+                const letterElement = document.getElementById(letterId);
+                letterElement.classList.add("animate__flipInX");
+                letterElement.style = `background-color:${tileColor};border-color:${tileColor}`;
+            }, interval * index);
+        });
+
+        guessedWordCount += 1;
 
         if (currentWord === word) {
             window.alert("Congragulations");
         }
 
         if (guessedWords.length === 6) {
-            window.alert('Sorry, you have no more guesses! The word is ${word}.');
+            window.alert(`Sorry, you have no more guesses! The word is ${word}.`);
         }
 
         guessedWords.push([]);
@@ -51,6 +84,7 @@ document.addEventListener("DOMContentLoaded", () => {
         for (let index = 0; index < 30; index++) {
             let square = document.createElement("div");
             square.classList.add("square");
+            square.classList.add("animate__animated")
             square.setAttribute("id", index + 1);
             gameBoard.appendChild(square);
         }
