34dd43a3d0c5200062944b74ca51d9fbd30feb80
Milindh Vijay Shajiv Nair <milindhvijay@gmail.com>
2022-03-06 20:29:53 +0530

    add all edge cases of core algorithm

--- src/word-util.test.ts
+++ src/word-util.test.ts
@@ -1,10 +1,73 @@
 import { describe, expect, it } from 'vitest'
-import { getRandomWord } from './word-utils'
+import { computeGuess, getRandomWord, LetterState } from './word-utils';
 import { render, screen} from './utils/test-utils'
 
-describe('word-utils', () => {
+describe('getRandomWord', () => {
   it('random word', () => {
     expect(getRandomWord()).toBeTruthy();
     expect(getRandomWord().length).toEqual(5);
+  })
+})
+
+
+describe('computeGuess', () => {
+  test('works with match and presents', () => {
+    expect(computeGuess('boost', 'basic')).toEqual([
+      LetterState.Match,
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Present,
+      LetterState.Miss,
+    ]);
+  });
+
+  it('it works with all matches', () => {
+    expect(computeGuess('boost', 'boost')).toEqual([
+      LetterState.Match,
+      LetterState.Match,
+      LetterState.Match,
+      LetterState.Match,
+      LetterState.Match,
+    ])
+  })
+
+  it('it works with full miss', () => {
+    expect(computeGuess('guard', 'boost')).toEqual([
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Miss,
+    ])
+  })
+
+  it('only deos one match when two letters are presnet', () => {
+    expect(computeGuess('solid', 'boost')).toEqual([
+      LetterState.Present,
+      LetterState.Match,
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Miss,
+    ])
+  })
+
+  it('when 2 letters are present but answer has only 1 of those letters', () => {
+    expect(computeGuess('allol', 'smelt')).toEqual([
+      LetterState.Miss,
+      LetterState.Present,
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Miss,
+    ])
+  })
+
+  it('when 1 letter matches but guess has more of the same letter', () => {
+    expect(computeGuess('allol', 'colon')).toEqual([
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Match,
+      LetterState.Match,
+      LetterState.Miss,
+    ])
   })
 })
--- src/word-utils.ts
+++ src/word-utils.ts
@@ -4,3 +4,62 @@ export function getRandomWord() {
     const randomIndex = Math.floor(Math.random() * wordBank.length)
     return wordBank[randomIndex]
 }
+
+export enum LetterState {
+    Miss,
+    Present,
+    Match
+}
+
+export function computeGuess(guess: string, answerString: string): LetterState[] {
+    const result: LetterState[] = []
+
+    if (guess.length !== answerString.length) {
+        return result
+    }
+
+    const guessAsArray = guess.split('')
+    const answer = answerString.split('');
+
+    const answerLetterCount: Record<string, number> = {}
+
+    guessAsArray.forEach((letter, index) => {
+        const currentAnswerLetter = answer[index]
+
+        answerLetterCount[currentAnswerLetter] = answerLetterCount[currentAnswerLetter] ? answerLetterCount[currentAnswerLetter] + 1 : 1
+
+        if (currentAnswerLetter === letter) {
+            result.push(LetterState.Match)
+        } else if (answer.includes(letter)) {
+            result.push(LetterState.Present)
+        } else {
+            result.push(LetterState.Miss)
+        }
+    })
+
+    result.forEach((curResult, resultIndex) => {
+        if (curResult !== LetterState.Present) {
+            return;
+        }
+
+        const guessLetter = guessAsArray[resultIndex]
+
+        answer.forEach((currentAnswerLetter, answerIndex) => {
+            if (currentAnswerLetter !== guessLetter) {
+                return
+            }
+
+            if (result[answerIndex] === LetterState.Match) {
+                result[resultIndex] = LetterState.Miss
+            }
+
+            if (answerLetterCount[guessLetter] <= 0) {
+                result[resultIndex] = LetterState.Miss
+            }
+        })
+
+        answerLetterCount[guessLetter]--
+    })
+
+    return result
+}
